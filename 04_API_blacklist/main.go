661c5467cb0217a4828ca94089971798ebd01eab
bibon228 <wzomkaw@gmail.com>
2026-03-22 15:15:44 +0300

    Api подключен

---
+++ 04_API_blacklist/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Visitors = map[string]bool
+
+var myVisitors = Visitors{
+	"Sex":  true,
+	"Flex": true,
+}
+
+func checkAccess(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	_, ok := myVisitors[name]
+	if ok {
+		fmt.Fprintf(w, "Доступ пользователю %s запрещен!\n", name)
+	} else {
+		fmt.Fprintf(w, "Добро пожаловать, %s!", name)
+	}
+}
+
+func main() {
+	http.HandleFunc("/check", checkAccess)
+	fmt.Println("Сервер запущен на :8080")
+	http.ListenAndServe(":8080", nil)
+}
