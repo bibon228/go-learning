9e30e42b7c4a67cf9aed6172307f69ff4cbee548
bibon228 <wzomkaw@gmail.com>
2026-03-22 15:33:20 +0300

    проверка и добавление в бан-лист через API

--- 04_API_blacklist/main.go
+++ 04_API_blacklist/main.go
@@ -18,11 +18,20 @@ func checkAccess(w http.ResponseWriter, r *http.Request) {
 	if ok {
 		fmt.Fprintf(w, "Доступ пользователю %s запрещен!\n", name)
 	} else {
-		fmt.Fprintf(w, "Добро пожаловать, %s!", name)
+		fmt.Fprintf(w, "Добро пожаловать %s\n", name)
 	}
 }
-
+func addToBlacklist(w http.ResponseWriter, r *http.Request) {
+	newName := r.URL.Query().Get("newName")
+	if newName == "" {
+		fmt.Fprintf(w, "Введите имя!")
+	} else {
+		myVisitors[newName] = true
+		fmt.Fprintf(w, "Пользователь %s добавлен в blacklist!", newName)
+	}
+}
 func main() {
+	http.HandleFunc("/add", addToBlacklist)
 	http.HandleFunc("/check", checkAccess)
 	fmt.Println("Сервер запущен на :8080")
 	http.ListenAndServe(":8080", nil)
